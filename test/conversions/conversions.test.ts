ba182718b89625caed584dcef71a4fce4941dbb8
Frank Kulak <fs.kulak@gmail.com>
2022-06-10 15:50:40 -0400

    Update image > DDS tests.

--- test/conversions/conversions.test.ts
+++ test/conversions/conversions.test.ts
@@ -1,8 +1,17 @@
+import fs from "fs";
+import path from "path";
+import { DdsImage } from "../../dst/images";
+
+const srcDir = path.resolve(__dirname, ".", "images");
+const outDir = path.resolve(__dirname, ".", "out");
+if (!fs.existsSync(srcDir)) fs.mkdirSync(srcDir, { recursive: true });
+if (!fs.existsSync(outDir)) fs.mkdirSync(outDir, { recursive: true });
+
 /*
-  This file takes the images that exist in the "images" directory, converts them
-  to unshuffled DDS images (DXT compression), and outputs them to the "out"
-  directory. It should be verified that the DDS images that are produced are
-  valid images around 22KB, and are able to be rendered.
+  The below code takes the PNG, JPG, GIF, TIFF, and BMP images that exist in the
+  "images" directory, converts them to unshuffled DDS images (DXT compression),
+  and outputs them to the "out" directory. It should be verified that these are
+  valid DDS images around 22KB, and are able to be rendered.
 
   DXT is a lossy compression format, so lower quality is expected for all
   conversions. PNG, GIF, and TIFF images should retain any transparency they
@@ -12,23 +21,25 @@
   expected and accepted.
 */
 
-import fs from "fs";
-import path from "path";
-import { DdsImage } from "../../dst/images";
-
-const srcDir = path.resolve(__dirname, ".", "images");
-const outDir = path.resolve(__dirname, ".", "out");
-
-if (!fs.existsSync(srcDir)) fs.mkdirSync(srcDir, { recursive: true });
-if (!fs.existsSync(outDir)) fs.mkdirSync(outDir, { recursive: true });
-
 ["png", "jpg", "bmp", "gif", "tiff"].forEach(ext => {
   const srcPath = path.join(srcDir, `LB.${ext}`);
   const outPath = path.join(outDir, `LB-${ext}.dds`);
   const buffer = fs.readFileSync(srcPath);
-
-  DdsImage.fromImage(buffer)
+  DdsImage.fromImageAsync(buffer)
     .then(dds => {
       fs.writeFileSync(outPath, dds.buffer);
     });
 });
+
+/*
+  TODO:
+*/
+
+// const dxtSrcPath = path.join(srcDir, "LB.dds");
+// const dxtOutPath = path.join(outDir, "LB-dxt.png");
+// const dxtBuffer = fs.readFileSync(dxtSrcPath);
+
+// DdsImage.fromImage(dxtBuffer)
+//   .then(dxt => {
+//     fs.writeFileSync(dxtOutPath, dxt.toImage());
+//   });
