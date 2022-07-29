9c19bb83d7c0b436b52187bc1bce1c4c9c454b4d
Frank Kulak <fs.kulak@gmail.com>
2022-07-29 11:14:09 -0400

    Move conversion options to types file.

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -6,3 +6,24 @@ export interface Bitmap {
   width: number;
   height: number;
 }
+
+/**
+ * Optional arguments to provide when creating a DDS image from another format.
+ */
+export type DdsConversionOptions = Partial<{
+  /**
+   * The maximum number of mipmaps to generate for a DDS image. This must be an
+   * integer between 1 and 15. If not provided, it is 15 by default.
+   * 
+   * This value includes the largest, highest-quality version of the image. To
+   * avoid scaling down the image at all, use a value of 1.
+   */
+  maxMipMaps: number;
+
+  /**
+   * Whether or not the resulting DDS image should be shuffled. If true, the DDS
+   * image will use DST5 compression. If false, it will use DXT5 compression.
+   * If not provided, it is false by default.
+   */
+  shuffle: boolean;
+}>;
